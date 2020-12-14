ab3c61888b01789e67a43c200e7de7f27c3418b6
mito343 <webdazer0@gmail.com>
2020-12-15 00:37:56 +0100

    added cloudinary to upload images

--- backend/routes/books.js
+++ backend/routes/books.js
@@ -1,27 +1,45 @@
-const { Router } = require('express');
+const { Router } = require("express");
 const router = Router();
-const { unlink } = require('fs-extra');
-const path = require('path');
+const { unlink } = require("fs-extra");
+const path = require("path");
 
-const Book = require('../models/Book');
+const Book = require("../models/Book");
 
-router.get('/', async (req, res) => {
-    const books = await Book.find();
-    res.json(books);
+const cloudinary = require("cloudinary");
+
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
 });
 
-router.post('/', async (req, res) => {
-    const { title, author, isbn } = req.body;
-    const imagePath = '/uploads/' + req.file.filename;
-    const newBook = new Book({ title, author, isbn, imagePath });
-    await newBook.save();
-    res.json({ message: 'Book Saved' });
+router.get("/", async (req, res) => {
+  const books = await Book.find();
+  res.json(books);
 });
 
-router.delete('/:id', async (req, res) => {
-    const book = await Book.findByIdAndDelete(req.params.id);
-    unlink(path.resolve('./backend/public' + book.imagePath));
-    res.json({ message: 'Book Deleted' });
+router.post("/", async (req, res) => {
+  const { title, author, isbn } = req.body;
+  // console.log(req.file);
+  const result = await cloudinary.v2.uploader.upload(req.file.path);
+  // console.log(result);
+  const newBook = new Book({
+    title,
+    author,
+    isbn,
+    imagePath: result.secure_url,
+    public_id: result.public_id,
+  });
+  await newBook.save();
+  await unlink(req.file.path);
+  res.json({ message: "Book Saved" });
+});
+
+router.delete("/:id", async (req, res) => {
+  const book = await Book.findByIdAndDelete(req.params.id);
+  const result = await cloudinary.v2.uploader.destroy(book.public_id);
+  console.log(result);
+  res.json({ message: "Book Deleted" });
 });
 
 module.exports = router;
